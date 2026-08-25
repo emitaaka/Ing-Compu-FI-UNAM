f76ae2d0df9bb83506a39250e835858f2db84cfd
emitaaka <emi.taka06@gmail.com>
2026-08-25 15:36:05 -0600

    tarea de ddm

---
+++ Quinto semestre/Diseño Digital Moderno/programas/baseABase.py
@@ -0,0 +1,51 @@
+#**Tarea**: Programa que convierta números de cualquiera base a otra base cualquiera xd yy palique complemento a 2(?) <---- solo cuando es binario?
+#ruta absoluta: cd downloads/ing-compu-fi-unam/quinto semestre/diseño digital moderno/programas
+import math
+
+numAConv = input("Ingresa el numero a covertir: ")
+
+try:
+    baseNumAConv = int(input("Ingresa la base de ese número: "))
+except:
+    print("Ingresa una base válida, por favor")
+
+try:
+    baseConv = int(input("Ingresa la base a la que quieres convertir el número: "))
+except:
+    print("Ingresa una base válida, por favor")
+
+print(f'Ingresaste el numero: {numAConv} en base {baseNumAConv} y quieres convertirlo a base {baseConv}')
+
+if baseNumAConv != 10:
+    tam = len(numAConv)
+    ent = ""
+    dec = ""
+    alreves = numAConv[::-1]
+    for i in range(tam):
+        agregar = ""
+        print("\n")
+        print(i, numAConv[i], baseNumAConv**i)
+        agregar = int(alreves[i]) * baseNumAConv**i
+        ent += str(agregar)
+        print()
+    print(ent)
+
+### Ideas ideas ideas
+#- en python
+#- primero pedir el número
+#- luego pedir la base en que está
+#- luego pedir la base a convertir
+#- si es de cualquier base a decimal, aplicar la fórmula de la sumatoria
+#- ¿cómo? primero dividimos el número (agarramos cada carácter. ocupando la string como arreglo(???)), luego lo convertimos a entero, usamos un for que se repita la cantidad de caracteres que tenga (longitud de la cadena), multiplicamos el primer valor d la cadena por la base potenciada al valor de su posición menos 1 (me entiendo yo) y guardamos ese valor en una variable llamada enteros
+#- por qué se llama enteros? por si el número tiene decimal, en este caso dividimos el número en dos cachos, parte entera y parte decimal
+#- para la parte entera, una vez que haya acabado de hacer la suma, entonces invierta la cadena y ya quedó convertido
+#- para la parte decimal, solo añade los números y ya queda en orden
+#- validar que sí sean números los que ingrese el usuario
+#- validar que el número sea válido en la base
+
+
+#IP 132.248.67.26 (cambia esta)
+#132.248.204.1
+#132.248.10.2
+#132.248.67.254
+#mascaras 255*3
