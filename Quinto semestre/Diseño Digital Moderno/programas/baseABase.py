1ced5e057c5b37e1cd3993c264a077ac08afc2ad
emitaaka <emi.taka06@gmail.com>
2026-08-30 16:53:08 -0600

    actualización semanal 30.07.2026

--- Quinto semestre/Diseño Digital Moderno/programas/baseABase.py
+++ Quinto semestre/Diseño Digital Moderno/programas/baseABase.py
@@ -1,5 +1,5 @@
 #**Tarea**: Programa que convierta números de cualquiera base a otra base cualquiera xd yy palique complemento a 2(?) <---- solo cuando es binario?
-#ruta absoluta: cd downloads/ing_compu/ing-compu-fi-unam/quinto semestre/diseño digital moderno/programas
+#ruta absoluta: cd downloads/ing-compu-fi-unam/quinto semestre/diseño digital moderno/programas
 letrasxd = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 
 #Convertir parte entera a base 10
@@ -58,63 +58,139 @@ def decFraccionACualquier(num, base):
             valor += str(ent)
         cont += 1
     return valor
-#Guardar número a convertir
-numAConv = input("Ingresa el numero a covertir: ")
 
-#Validamos que la base sea un número entero (entero?)
-try:
-    baseOri = int(input("Ingresa la base de ese número: "))
-except ValueError:
-    baseOri = int(input("Ingresa una base válida, por favor: "))
+def validarBinario(binario):
+    for i in range(len(binario)):
+        try:
+            if int(binario[i]) > 1:
+                print(f"El número {binario} es inválido.")
+                break
+        except ValueError:
+            print("Error al revisar el código")
 
-nuevoNumAConv = list(numAConv)
-for i in range(len(numAConv)):
-    if numAConv[i] != '.':
-        caracter = numAConv[i].upper()
-        if caracter in letrasxd:
-            nuevoNumAConv[i] = letrasANumeros(caracter)
-        elif caracter.isdigit():
-            nuevoNumAConv[i] = int(caracter)
-        else:
-            print(f'{caracter} no es un dígito válido.')
-            break
-        if int(nuevoNumAConv[i]) > baseOri - 1:
-           print(f"{caracter} no es un dígito válido.")
-        
-try:
-    baseConv = int(input("Ingresa la base a la que quieres convertir el número: "))
-except:
-    print("Ingresa una base válida, por favor")
+def tienePunto(numero):
+    punto = numero.find('.')
 
-tam = len(nuevoNumAConv)
-entero = ""
-decimal = ""
-total = ""
+    ent = ""
+    dec = ""
 
-#Separando entero de decimal y convirtiendo la base
-posPunto = numAConv.find('.')
-if posPunto != -1:
-    entero = nuevoNumAConv[:posPunto]
-    decimal = nuevoNumAConv[posPunto + 1:]
-    total += str(entDec(entero, baseOri) + decDiez(decimal, baseOri))
-else:
-    entero = nuevoNumAConv
-    total += str(entDec(entero, baseOri))
-
-if baseConv == 10:
-    print(f"El numero convertido a base 10 es: {total}")
-else:
-    posPunto2 = total.find(".")
-    aConv = total
-    if posPunto2 != -1:
-        entero = int(aConv[:posPunto2])
-        decimal = int(aConv[posPunto2 + 1:])
-        total = str(decEnteroACualquier(entero, baseConv)) + '.' + (decFraccionACualquier(decimal, baseConv))
-        print(f'El número {numAConv} convertido a base {baseConv} es: {total}')
+    if punto != -1:
+        ent += numero[:punto]
+        dec += numero[punto + 1:]
     else:
-        entero = int(aConv)
-        total = decEnteroACualquier(entero, baseConv)
-        print(f'El número {numAConv} convertido a base {baseConv} es: {total}')
+        ent = numero
+
+    return ent, dec
+
+opc = 0
+
+while opc != 3:
+    print("\nBienvenido al conversor de bases, ¿qué deseas hacer?")
+    print("1. Convertir un número de cualquier base a cualquier base.")
+    print("2. Sumar (o restar) números (solo en binario).")
+    print("3. Salir")
+    opc = int(input())
+
+    if opc == 1:
+        print(f'Elegiste la opción {opc}: Convertir números.')
+        #Guardar número a convertir
+        numAConv = input("Ingresa el numero a covertir: ")
+
+        #Validamos que la base sea un número entero (entero?)
+        try:
+            baseOri = int(input("Ingresa la base de ese número: "))
+        except ValueError:
+            baseOri = int(input("Ingresa una base válida, por favor: "))
+
+        nuevoNumAConv = list(numAConv)
+        for i in range(len(numAConv)):
+            if numAConv[i] != '.':
+                caracter = numAConv[i].upper()
+                if caracter in letrasxd:
+                    nuevoNumAConv[i] = letrasANumeros(caracter)
+                elif caracter.isdigit():
+                    nuevoNumAConv[i] = int(caracter)
+                else:
+                    print(f'{caracter} no es un dígito válido.')
+                    break
+                if int(nuevoNumAConv[i]) > baseOri - 1:
+                    print(f"{caracter} no es un dígito válido.")
+                
+        try:
+            baseConv = int(input("Ingresa la base a la que quieres convertir el número: "))
+        except:
+            print("Ingresa una base válida, por favor")
+
+        tam = len(nuevoNumAConv)
+        entero = ""
+        decimal = ""
+        total = ""
+
+        #Separando entero de decimal y convirtiendo la base
+        posPunto = numAConv.find('.')
+        if posPunto != -1:
+            entero = nuevoNumAConv[:posPunto]
+            decimal = nuevoNumAConv[posPunto + 1:]
+            total += str(entDec(entero, baseOri) + decDiez(decimal, baseOri))
+        else:
+            entero = nuevoNumAConv
+            total += str(entDec(entero, baseOri))
+
+        if baseConv == 10:
+            print(f"El numero convertido a base 10 es: {total}")
+        else:
+            posPunto2 = total.find(".")
+            aConv = total
+            if posPunto2 != -1:
+                entero = int(aConv[:posPunto2])
+                decimal = int(aConv[posPunto2 + 1:])
+                total = str(decEnteroACualquier(entero, baseConv)) + '.' + (decFraccionACualquier(decimal, baseConv))
+                print(f'El número {numAConv} convertido a base {baseConv} es: {total}')
+            else:
+                entero = int(aConv)
+                total = decEnteroACualquier(entero, baseConv)
+                print(f'El número {numAConv} convertido a base {baseConv} es: {total}')
+    elif opc == 2:
+        print(f'Elegiste la opción {opc}: Suma de números binarios.\nTen en cuenta que el bit más significativo será considerado el bit de signo.\n')
+
+        binario1 = input("Ingresa el primer número en binario: ")
+        binario2 = input("Ingresa el segundo número en binario: ")
+
+        entBinario1, decBinario1 = tienePunto(binario1)
+        entBinario2, decBinario2 = tienePunto(binario2)
+
+        lenBin1 = len(binario1)
+        lenBin2 = len(binario2)
+
+        if lenBin1 != lenBin2:
+            print("los numeros son de diferente longitud, ahorita lo arreglamos")
+            dif = lenBin1 - lenBin2
+            ceros = ""
+            if dif < 0:
+                dif *= -1
+            for i in range(dif):
+                ceros += "0"
+            if lenBin1 < lenBin2:
+                temp = binario1[1:]
+                binario1 = binario1[:1] + ceros + temp
+                print(f"actualizamos binario1 y quedó así: {binario1}, temp vale {temp} y ceros vale {ceros}")
+            else: 
+                temp = binario2[1:]
+                binario2 = binario2[:1] + ceros + temp
+                print(f"actualizamos binario2 y quedó así: {binario2}, temp vale {temp} y ceros vale {ceros}")
+
+        if binario2[0] == '1':
+            print("es negativo :D")
+            for i in range(len(binario2) - 1, 0, -1):
+                print(f'{binario2[i]}')
+
+    elif opc == 3:
+        print("Adiós!")
+    else:
+        print("Ingresa un número válido por favor")
+
+
+
 
 ### Ideas ideas ideas
 #- en python
